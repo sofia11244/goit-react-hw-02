f0300cd6e4ffae6d5c0e73667bd14789023f1f77
sofia11244 <denizsofia03@gmail.com>
2024-10-30 13:52:27 +0300

    buttons added texts eddited reset button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,60 @@
 import { useState } from 'react'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+
 
   return (
     <>
-      <div>
+      <div className='description'>
+      <h1 className='title'>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the options below.
+      </p>
+
       </div>
-      <h1>Counting</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
+      
+
+
+      <div className='options'>
         
-        </p>
+        <button onClick={() => setGood(good + 1)}>
+          Good {good}
+        </button>
+
+        <button onClick={() => setNeutral(neutral + 1)}>
+          Neutral {neutral}
+        </button>
+        
+        <button onClick={() => setBad(bad + 1)}>
+          Bad {bad}
+        </button>
+        <button onClick={() => {
+            setGood(0);
+            setNeutral(0);
+            setBad(0);
+        }}>
+            Reset
+        </button>
+        
       </div>
-      <p className="read-the-docs">
+      <p className='feedback'>
+        No feedback yet
+        {/* change to total of the clicks 
+        
+
+        const [good, setGood] = useState(0)
+        const [neutral, setNeutral] = useState(0)
+        const [bad, setBad] = useState(0)
+
+        const totalFeedback = good + neutral + bad
+
+        Math.round((good / totalFeedback) * 100)
+
+        */}
       </p>
     </>
   )
