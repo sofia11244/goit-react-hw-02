f49907f37f879c6370d70fdab0f0922fc6237023
sofia11244 <denizsofia03@gmail.com>
2024-10-30 15:11:17 +0300

    styles done local stroge done reset button func. done totals added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [good, setGood] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [neutral, setNeutral] = useState(0)
+  const [good, setGood] = useState(() => {
+    return Number(localStorage.getItem("good")) || 0
+  });
+
+  const [bad, setBad] = useState(() => {
+    return Number(localStorage.getItem("bad")) || 0
+  });
+
+  const [neutral, setNeutral] = useState(() => {
+    return Number(localStorage.getItem("neutral")) || 0
+  });
+
+  const totalFeedback = good + bad;
+  const positivePercentage = totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+
+
+  useEffect(() => { //her bir geri bildirim durumu değiştiğinde yerel depolamayı güncellemek için
+    localStorage.setItem("good", good);
+    localStorage.setItem("bad", bad);
+    localStorage.setItem("neutral", neutral);
+  }, [good, bad, neutral]);
 
 
   return (
-    <>
+    <>                  
       <div className='description'>
       <h1 className='title'>Sip Happens Café</h1>
       <p>
         Please leave your feedback about our service by selecting one of the options below.
       </p>
-
       </div>
       
-
-
       <div className='options'>
         
         <button onClick={() => setGood(good + 1)}>
@@ -36,28 +51,31 @@ function App() {
             setGood(0);
             setNeutral(0);
             setBad(0);
+            localStorage.removeItem("good");
+            localStorage.removeItem("neutral");
+            localStorage.removeItem("bad");
         }}>
             Reset
         </button>
-        
       </div>
-      <p className='feedback'>
-        No feedback yet
-        {/* change to total of the clicks 
-        
 
-        const [good, setGood] = useState(0)
-        const [neutral, setNeutral] = useState(0)
-        const [bad, setBad] = useState(0)
-
-        const totalFeedback = good + neutral + bad
-
-        Math.round((good / totalFeedback) * 100)
-
-        */}
-      </p>
-    </>
-  )
-}
+      <div className='feedback'>
+        <ul className='feedback-items'>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral} </li>
+          <li>Bad: {bad}</li>
+          {good + neutral + bad === 0 
+          ? <li> No feedback yet</li>
+            : (
+            <>
+              <li>Total feedback: {good + neutral + bad} </li>
+              <li> Positive feedback: {positivePercentage}%</li>
+            </>
+            )}
+          </ul>
+        </div>
+      </>
+    )
+  }
 
 export default App
